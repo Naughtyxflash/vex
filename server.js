4f8c6d0128e2db60aa22091aea7c2cb647fd1cf4
greatvisionapp-arch <greatvisionapp@gmail.com>
2026-04-24 02:22:42 +0530

    fix port  lund nhi hoga

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 // ENV
 const API_KEY = process.env.API_KEY;
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 if (!PORT) {
   console.error("FATAL: PORT not provided");
